36f25a8921eb50c952f78983d069d8f29c8c8279
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-15 16:51:39 +0300

    Первая дополнительная задача решена

---
+++ lesson_3/hw07_3.py
@@ -0,0 +1,21 @@
+# Задача: написать функцию которая на вход принимает список с неопределенным уровнем вложенности других списков,
+# например  [1,  2, [3, 4, [5]], 6, [[[7, [8]]]]]
+# Функция должна вернуть плоский список сохраняя последовательность [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def list_convert(outerlist, item_to_check):
+    print(f"Рассматриваем элемент (начало): {item_to_check}")
+    if type(item_to_check) is list:
+        for i in item_to_check:
+            print(f"Рассматриваем элемент при вызове функции: {i}")
+            list_convert(outerlist, i)
+    else:
+        print(f"добавляем в список: {item_to_check}")
+        outerlist.append(item_to_check)
+
+
+mylist = []
+list_to_plain = [1,  2, [3, 4, [5]], 6, [[[7, [8]]]]]
+list_convert(mylist, list_to_plain)
+
+print(mylist)
