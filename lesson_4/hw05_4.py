b7ec61b52572ffa48cafd43a0e0f85f7fd0b5d15
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-18 11:34:09 +0300

    Пятая задача сделана

--- lesson_4/hw05_4.py
+++ lesson_4/hw05_4.py
@@ -3,3 +3,13 @@
 # Подсказка: использовать функцию reduce().
 
 
+from functools import reduce
+
+base_list = [item for item in range(100, 1001) if item % 2 == 0]
+
+
+def my_func(prev_item, item):
+    return prev_item * item
+
+
+print(f'Произведение всех чисел в списке: {reduce(my_func, base_list)}')
