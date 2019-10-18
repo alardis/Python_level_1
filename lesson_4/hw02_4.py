5975730a5721ab3bca369da5602ad81e5f9dab26
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-18 11:02:08 +0300

    Вторая задача сделана

--- lesson_4/hw02_4.py
+++ lesson_4/hw02_4.py
@@ -2,3 +2,15 @@
 # Подсказка: элементы, удовлетворяющие условию, оформить в виде списка. Для формирования списка использовать генератор.
 
 
+import random
+
+
+base_list = [random.randint(0, 10) for _ in range(10)]
+print(f'Рандомный лист: {base_list}')
+
+new_list = []
+for num, item in enumerate(base_list):
+    if num > 0 and base_list[num-1] < item:
+        new_list.append(item)
+
+print(f'Новый список: {new_list}')
