717a632c8549c9a9cb54d7ead997a4359b2dda4e
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-22 11:24:11 +0300

    Вторая задача сделана

--- lesson_5/hw02_5.py
+++ lesson_5/hw02_5.py
@@ -2,3 +2,13 @@
 # количества слов в каждой строке.
 
 
+import os
+
+with open(f'{os.path.abspath("Python_level_1/lesson_5")}/new_file.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for num, line in enumerate(lines):
+        if line.isspace():
+            continue
+        else:
+            words = line.strip().split(' ')
+            print(f'Строка {num}: количество слов -- {len(words)}')
