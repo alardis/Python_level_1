7ae897a71ea541c01a2d90599b3b214b1a6bfa32
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-22 10:54:51 +0300

    Первая задача сделана

--- lesson_5/hw01_5.py
+++ lesson_5/hw01_5.py
@@ -1,4 +1,10 @@
 # Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных свидетельствует пустая строка.
 
+import os
 
+with open(f'{os.path.abspath("Python_level_1/lesson_5")}/new_file.txt', 'w', encoding='utf-8') as file:
+    line = None
+    while line != '':
+        line = input('Введите строку, которую хотите записать в файл. Если не хотите, нажмите Enter\n')
+        file.write(f'{line}\n')
