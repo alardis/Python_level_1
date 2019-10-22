7cc9d4b92ef0c304025c8654aa1094acf0932771
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-22 11:51:35 +0300

    Третья задача сделана

--- lesson_5/hw03_5.py
+++ lesson_5/hw03_5.py
@@ -3,3 +3,23 @@
 # Выполнить подсчет средней величины дохода сотрудников.
 
 
+import os
+
+with open(f'{os.path.abspath("Python_level_1/lesson_5")}/new_file.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    av_sum = 0
+    for num, line in enumerate(lines):
+        if line.isspace():
+            continue
+        else:
+            line = line.strip()
+            words = line.split(' ')
+            try:
+                salary = int(words[1])
+                av_sum += salary
+                if salary <= 20000:
+                    print(f'Сотрудник {words[0]} имеет низкую зарплату ({words[1]})')
+            except ValueError:
+                print(f'Проверьте данные в файле на строке {num}')
+
+    print(f'\nСредняя зарплата при этом составляет: {av_sum / len(lines)}')
