7d8ecacd10658dbb7e6248a21361417fde6af953
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-23 17:39:40 +0300

    Седьмая задача сделана

--- lesson_5/hw07_5.py
+++ lesson_5/hw07_5.py
@@ -5,5 +5,33 @@
 # Итоговый список сохранить в файл.
 # Подсказка: использовать менеджеры контекста.
 
+# формат записи:
+# Рога и Копыта, ООО, 100000, 4788
 
+import os
 
+with open(f'{os.path.abspath("Python_level_1/lesson_5")}/new_file.txt', 'r', encoding='utf-8') as file,\
+        open(f'{os.path.abspath("Python_level_1/lesson_5")}/new_new_file.txt', 'a+', encoding='utf-8') as new_file:
+    data_list = file.readlines()
+    # print(data_list)
+    whole_list = []
+    av_income = 0
+    for item in data_list:
+        local_data_list = item.split(', ')
+        income = 0
+        try:
+            income = int(local_data_list[2]) - int(local_data_list[3])
+        except ValueError:
+            print('Проверьте финансовые показатели организации!')
+        whole_list.append({'Название': f'{local_data_list[1]} {local_data_list[0]}', 'Прибыль': income})
+        av_income += income
+    av_income = av_income / len(data_list)
+    whole_list.append({'Средняя прибыль': av_income})
+    # print(whole_list)
+
+    for item in whole_list:
+        for key, value in item.items():
+            line = f'{key}: {value}'
+            # print(line)
+            new_file.write(f'{line}\n')
+        new_file.write('\n')
