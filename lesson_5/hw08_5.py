8c865996ab06fa57f9f7b2c45ad718c93d051544
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-24 16:07:10 +0300

    Восьмая задача сделана

--- lesson_5/hw08_5.py
+++ lesson_5/hw08_5.py
@@ -4,3 +4,14 @@
 # Наша задача определить именно букву которая чаще всего встречается в строке
 
 
+from collections import Counter
+
+
+often_letter_2 = lambda line: max(zip(
+    (lambda local_line: {key: value for key, value in Counter(line.lower()).items()
+                         if not key.isdigit() and not key.isspace()})(line).values(),
+    (lambda local_line: {key: value for key, value in Counter(line.lower()).items()
+                         if not key.isdigit() and not key.isspace()})(line).keys()
+))[1]
+
+print(often_letter_2('Раз два три четыре пять'))
