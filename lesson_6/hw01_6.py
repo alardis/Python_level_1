9b70d17f528c0256fa616fee62e25bfa181dff20
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-25 11:15:39 +0300

    Первая задача сделана

--- lesson_6/hw01_6.py
+++ lesson_6/hw01_6.py
@@ -4,3 +4,30 @@
 # Проверить работу примера, создав экземпляр и вызвав описанный метод.
 
 
+import time
+
+class TrafficLight:
+    __color = None
+
+    def running(self):
+        time_to_start = time.time()
+        current_time = None
+        __color = 'красный'
+        print(f'Режим светофора: {__color}')
+
+        while __color != 'зеленый':
+            current_time = time.time()
+            if current_time - time_to_start == 1:
+                print(time.time())
+
+            if current_time - time_to_start >= 7 and __color == 'красный':
+                __color = 'желтый'
+                print(f'Режим светофора: {__color}')
+                time_to_start = time.time()
+            elif current_time - time_to_start >= 2 and __color == 'желтый':
+                __color = 'зеленый'
+                print(f'Режим светофора: {__color}')
+
+
+light = TrafficLight()
+light.running()
