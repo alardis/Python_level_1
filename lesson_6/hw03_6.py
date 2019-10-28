383df23b217b0df4e88107aa3e26abdba200dc3c
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-28 16:11:22 +0300

    Третья задача сделана

--- lesson_6/hw03_6.py
+++ lesson_6/hw03_6.py
@@ -6,3 +6,34 @@
 # передать данные, проверить значения атрибутов, вызвать методы экземпляров).
 
 
+class Worker:
+    name: str
+    surname: str
+    position: str
+    _income: dict
+
+    def __init__(self, name, surname, position, profit, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'profit': profit, 'bonus': bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, profit, bonus):
+        super().__init__(name, surname, position, profit, bonus)
+
+    def get_full_name(self):
+        print(f'Полное имя сотрудника: {self.name} {self.surname}')
+
+    def get_full_profit(self):
+        full_profit = 0
+        for key, value in self._income.items():
+            full_profit += value
+        print(f'Доход с учетом премии: {full_profit}')
+
+
+position_test = Position('Андрей', 'Иванов', 'менеджер', 400, 100)
+position_test.get_full_name()
+position_test.get_full_profit()
