0122cabed4905e050f775c5859cc6f6c87ef334d
VIZEX\ashumov <ashumov@vizex.ru>
2019-10-29 13:39:48 +0300

    Третья задача сделана

--- lesson_7/hw03_7.py
+++ lesson_7/hw03_7.py
@@ -10,3 +10,69 @@
 # Подсказка: подробный список операторов для перегрузки доступен по ссылке.
 
 
+class Cell:
+    __how_many: int
+
+    @property
+    def how_many(self):
+        return self.__how_many
+
+    @how_many.setter
+    def how_many(self, value):
+        if value > 0:
+            self.__how_many = value
+        else:
+            raise ValueError('Количество клеток не может быть больше нуня!')
+
+    def __init__(self, how_many):
+        self.__how_many = how_many
+
+    def make_cell(self, number: int):
+        result = ''
+        if self.how_many < number:
+            result = '*' * self.how_many
+        else:
+            reserve = self.how_many
+            while reserve > 0:
+                result = f'{result}{"*" * min(number, reserve)}\n'
+                reserve = reserve - number
+            result = result.strip('\n')
+            return result
+
+    def __str__(self):
+        return f'Набор ячеек, количество: {self.how_many}'
+
+    def __add__(self, other):
+        if not isinstance(other, Cell):
+            raise ValueError('Второе слагаемое не является ячейками!')
+
+        return Cell(self.how_many + other.how_many)
+
+    def __sub__(self, other):
+        if not isinstance(other, Cell):
+            raise ValueError('Второе слагаемое не является ячейками!')
+        elif self.how_many - other.how_many < 0:
+            raise ValueError('Ячеек во втором объекте больше, чем в первом. Недопустимая операция')
+        return Cell(self.how_many - other.how_many)
+
+    def __mul__(self, other):
+        if isinstance(other, Cell):
+            return Cell(self.how_many * other.how_many)
+        elif isinstance(other, int):
+            return Cell(self.how_many * other)
+        else:
+            raise ValueError('Недопустимый формат множителя')
+
+    def __truediv__(self, other):
+        if isinstance(other, Cell):
+            return Cell(self.how_many // other.how_many)
+        elif isinstance(other, int):
+            return Cell(self.how_many // other)
+        else:
+            raise ValueError('Недопустимый формат делителя')
+
+
+cells = Cell(15)
+cells1 = Cell(6)
+print(cells.make_cell(6))
+print(cells / cells1)
