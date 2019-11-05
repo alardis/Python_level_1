f6f0bafa6d5da78a584cb14a046e1b29266460da
VIZEX\ashumov <ashumov@vizex.ru>
2019-11-05 10:54:45 +0300

    Второе задание выполнено

--- lesson_8/hw02_8.py
+++ lesson_8/hw02_8.py
@@ -3,3 +3,18 @@
 # эту ситуацию и не завершиться с ошибкой.
 
 
+class CustomZeroDivide(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+try:
+    one = int(input('Введите первое число:\n'))
+    two = int(input('Введите второе число:\n'))
+    if two == 0:
+        raise CustomZeroDivide('Похоже, вы тут делаете то, что нельзя')
+    three = one / two
+    print(f'{one} / {two} = {one/two}')
+except CustomZeroDivide:
+    print('Пожалуйста, не делите на ноль. Так нельзя\n')
+    print(f'{one} / {two} = ...')
