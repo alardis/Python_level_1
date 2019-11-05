55bc89697059fa965c98dfebfed6efe2fc93d2fb
VIZEX\ashumov <ashumov@vizex.ru>
2019-11-05 17:42:05 +0300

    Четвертое задание выполнено

--- lesson_8/hw04_8.py
+++ lesson_8/hw04_8.py
@@ -4,3 +4,99 @@
 # уникальные для каждого типа оргтехники.
 
 
+from abc import ABC
+
+
+class OfficeEquipment(ABC):
+    __id: int
+    __price: float
+    __title: str
+    __status: str
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        pass
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self.__price = value
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def id(self, value):
+        self.__title = value
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        self.__status = value
+
+    def __init__(self, id, title, price, status):
+        self.__id = id
+        self.__title = title
+        self.__price = price
+        self.__status = status
+
+
+class Printer(OfficeEquipment):
+    __cartridge_status: str
+
+    @property
+    def cartridge_status(self):
+        return self.__cartridge_status
+
+    @cartridge_status.setter
+    def cartridge_status(self, value):
+        self.__cartridge_status = value
+
+    def __init__(self, id, title, price, status):
+        super().__init__(id, title, price, status)
+
+
+class ScannerEquipment(OfficeEquipment):
+    __resource: int
+
+    @property
+    def resource(self):
+        return self.__resource
+
+    @resource.setter
+    def resource(self, value):
+        self.__resource = value
+
+    def __init__(self, id, title, price, status):
+        super().__init__(id, title, price, status)
+
+
+class PhotoCopier(OfficeEquipment):
+    __print_mode: str
+
+    @property
+    def print_mode(self):
+        return self.__print_mode
+
+    @print_mode.setter
+    def print_mode(self, value):
+        self.__print_mode = value
+
+    def __init__(self, id, title, price, status):
+        super().__init__(id, title, price, status)
+
+
+class WareHouse:
+    pass
